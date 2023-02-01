de3726a05e5da2e8a127a2950a4bcee324a3de8d
Boutahar Mohammed <mohammed.boutahar@mines-ales.org>
2023-02-01 17:30:27 +0100

    new start

--- snapface/src/app/app.module.ts
+++ snapface/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FaceSnapeComponent } from './face-snape/face-snape.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FaceSnapeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
